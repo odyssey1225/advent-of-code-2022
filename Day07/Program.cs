dcf31d4b3052b7a9cfacbb587c1c3b6c4a51d975
odyssey1225 <abholsopple@gmail.com>
2022-12-16 21:58:28 -0600

    Added Day 07 project.

---
+++ Day07/Program.cs
@@ -0,0 +1,7 @@
+﻿
+using var reader = new StreamReader(new FileInfo("SampleInput.txt").OpenRead());
+while (!reader.EndOfStream)
+{
+    var nextLine = reader.ReadLine();
+    if (nextLine is null) continue;
+}
