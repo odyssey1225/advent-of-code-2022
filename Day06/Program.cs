e934e6246a087aeac8adbfea4380c4ecde22e35b
Alex Holsopple <alex@Alexs-MacBook-Pro.local>
2022-12-16 21:25:36 -0600

    * Day 06 solution.

---
+++ Day06/Program.cs
@@ -0,0 +1,13 @@
+﻿using var reader = new StreamReader(new FileInfo("Input.txt").OpenRead());
+var commData = reader.ReadLine();
+
+// const int windowSize = 4; // Part 1
+const int windowSize = 14; // Part 2
+for (var i = windowSize; i < commData?.Length; i++)
+{
+    if (commData[(i - windowSize)..i].GroupBy(c => c).Count() == windowSize)
+    {
+        Console.WriteLine(i);
+        break;
+    }
+}
